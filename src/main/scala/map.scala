1c689697381eb5e9e160e5e517ce896f57620291
khivi <khivi@localhost>
2013-02-24 18:25:03 -0500

    Make it work on a generic iterable

--- src/main/scala/map.scala
+++ src/main/scala/map.scala
@@ -2,12 +2,12 @@ package com.khivi.collection
 
 package immutable
 
-class MultiValueMap[K, +V](underlying: Map[K, Seq[V]]) extends Map[K, Seq[V]] {
-  type SeqV = Seq[V]
-  type U = Map[K, SeqV]
+class MultiValueMap[K, +V](underlying: Map[K, Iterable[V]]) extends Map[K, Iterable[V]] {
+  type IterableV = Iterable[V]
+  type U = Map[K, IterableV]
   type This = MultiValueMap[K, V]
 
-  override def get(key: K): Option[SeqV] = underlying.get(key)
+  override def get(key: K): Option[IterableV] = underlying.get(key)
 
   override def iterator = underlying.iterator
 
@@ -17,34 +17,34 @@ class MultiValueMap[K, +V](underlying: Map[K, Seq[V]]) extends Map[K, Seq[V]] {
 
   override def size: Int = underlying.size
 
-  override def foreach[U](f: ((K, SeqV)) => U): Unit = underlying.foreach(f)
+  override def foreach[U](f: ((K, IterableV)) => U): Unit = underlying.foreach(f)
 
   // Implementation
-  def _add[SeqV1 >: SeqV](kv: (K, SeqV1)): This = {
+  def _add[V1, Iterable[V1] >: IterableV](kv: (K, Iterable[V1])): This = {
     val res = (underlying.get(kv._1) match {
-      case Some(sv) => sv ++ kv._2.asInstanceOf[SeqV]
-      case None => kv._2.asInstanceOf[SeqV]
+      case Some(sv) => sv ++ kv._2.asInstanceOf[IterableV]
+      case None => kv._2.asInstanceOf[IterableV]
     })
     MultiValueMap(underlying + ((kv._1, res)))
   }
-  def ++ [SeqV1 >: SeqV](kv: (K, SeqV1)): This = this._add(kv)
-  override def + [V1 >: SeqV](kv: (K, V1)): This = this._add((kv._1, Seq(kv._2)))
+  def ++ [V1, Iterable[V1] >: IterableV](kv: (K, Iterable[V1])): This = this._add(kv)
+  override def + [V1 >: IterableV](kv: (K, V1)): This = this._add((kv._1, Iterable(kv._2)))
 
-  def _rem[SeqV1 >: SeqV](kv: (K, SeqV1)): This = underlying.get(kv._1) match {
-      case Some(sv) =>  val kv2 = kv._2.asInstanceOf[SeqV]
-                        sv.filter(!kv2.contains(_)) match {
+  def _rem[V1, Iterable[V1] >: IterableV](kv: (K, Iterable[V1])): This = underlying.get(kv._1) match {
+      case Some(sv) =>  val rv = kv._2.asInstanceOf[IterableV].toSet
+                        sv.filter(!rv.contains(_)) match {
                           case Nil => MultiValueMap(underlying - kv._1)
                           case res => MultiValueMap(underlying + ((kv._1, res)))
                         }
       case None => this
   }
-  def --[SeqV1 >: SeqV](kv: (K, SeqV1)): This = this._rem(kv)
-  def -[V1 >: SeqV](kv: (K, V1)): This = this._rem((kv._1, Seq(kv._2)))
+  def --[V1, Iterable[V1] >: IterableV](kv: (K, Iterable[V1])): This = this._rem(kv)
+  def -[V1 >: IterableV](kv: (K, V1)): This = this._rem((kv._1, Iterable(kv._2)))
 
 }
 
 object MultiValueMap {
   private object EmptyMap extends MultiValueMap[Any, Nothing](Map.empty)
   def empty[K,V] = EmptyMap.asInstanceOf[MultiValueMap[K, V]]
-  def apply[K,V](underlying: Map[K, Seq[V]]) = new MultiValueMap(underlying)
+  def apply[K,V](underlying: Map[K, Iterable[V]]) = new MultiValueMap(underlying)
 }
